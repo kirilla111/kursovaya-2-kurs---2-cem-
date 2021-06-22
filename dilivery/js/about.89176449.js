8188a382d94364518b41e55d3d4ed45c0c4b001d
kirilla111 <kirill_afanasyev00@mail.ru>
2021-06-22 13:43:59 +0300

    production added

---
+++ dilivery/js/about.89176449.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{f820:function(n,t,a){"use strict";a.r(t);var c=a("7a23");const e={class:"about"},o=Object(c["h"])("h1",null,"This is an about page",-1);function s(n,t){return Object(c["q"])(),Object(c["d"])("div",e,[o])}const u={};u.render=s;t["default"]=u}}]);
+//# sourceMappingURL=about.89176449.js.map
